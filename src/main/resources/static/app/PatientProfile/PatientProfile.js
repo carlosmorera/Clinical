57e84811836a69925a6f7b627b2f6706c3231af5
carlosmorera1565 <carlos.morera@mail.escuelaing.edu.co>
2017-10-11 15:46:15 -0500

    Cambio fechas

--- src/main/resources/static/app/PatientProfile/PatientProfile.js
+++ src/main/resources/static/app/PatientProfile/PatientProfile.js
@@ -17,15 +17,27 @@ angular.module('myApp.PatientProfile', ['ngRoute'])
                                         function (value) {
                                             $scope.person = value;
                                             var fechaHoy = Date.now();
-                                            var edad_paciente = fechaHoy - $scope.person.fechaDeNacimiento;
+                                            var edadPacienteI = $scope.person.fechaDeNacimiento.split('-');
+                                            var edad_paciente = fechaHoy - new Date(edadPacienteI[0], edadPacienteI[1], edadPacienteI[2]);
                                             var ageDateP = new Date(edad_paciente);
-                                            $scope.edadPaciente = Math.abs(ageDateP.getUTCFullYear() - 1970);
-                                            var edad_madre = fechaHoy - $scope.person.fechaNacimientoMadre;
-                                            var edad_padre = fechaHoy - $scope.person.fechaNacimientoPadre;
-                                            var edadFechaM = new Date(edad_madre);
+                                            $scope.edadPaciente = Math.abs(ageDateP.getUTCFullYear() - 1970)
+
+
                                             var edadFechaP = new Date(edad_padre);
-                                            if($scope.person.fechaNacimientoMadre != null){$scope.edadMadre = Math.abs(edadFechaM.getUTCFullYear() - 1970)};
-                                            if($scope.person.fechaNacimientoPadre != null){$scope.edadPadre = Math.abs(edadFechaP.getUTCFullYear() - 1970)};
+                                            if ($scope.person.fechaNacimientoMadre !== null) {
+                                                var edadMI = $scope.person.fechaNacimientoMadre.split('-');
+                                                var edad_madre = fechaHoy - new Date(edadMI[0], edadMI[1], edadMI[2]);
+                                                var edadFechaM = new Date(edad_madre);
+                                                $scope.edadMadre = Math.abs(edadFechaM.getUTCFullYear() - 1970)
+                                            }
+                                            ;
+                                            if ($scope.person.fechaNacimientoPadre !== null) {
+                                                var edadPI = $scope.person.fechaNacimientoPadre.split('-');
+                                                var edad_padre = fechaHoy - new Date(edadPI[0], edadPI[1], edadPI[2]);
+                                                var edadFechaP = new Date(edad_padre);
+                                                $scope.edadPadre = Math.abs(edadFechaP.getUTCFullYear() - 1970)
+                                            }
+                                            ;
 
 
                                         },
